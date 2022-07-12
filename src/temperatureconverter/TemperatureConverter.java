1b1ff184e0307d50e4352f53aab3606c5ba5a4ca
Kevin Bell <bellKevin@pm.me>
2022-07-12 02:18:26 +0000

    Update TemperatureConverter.java

--- src/temperatureconverter/TemperatureConverter.java
+++ src/temperatureconverter/TemperatureConverter.java
@@ -1,55 +1,38 @@
+/** ****************************************************************************
+ * TemperatureConverter.java
+ * Kevin Bell
+ * This program practices the use of unit testing on a simple temperature
+ * program
+ **************************************************************************** */
 package temperatureconverter;
-
-/******************************************************************************
-* TemperatureConverter.java 
-* Programmer: @author jcboyd
-* Version: 1.0
-* Course: SDEV 2210
-* This program practices the use of unit testing on a simple temperature 
-* program
-******************************************************************************/
 
 import java.util.*; //Scanner
 
 public class TemperatureConverter {
 
     public static void main(String[] args) {
-
-        System.out.println("Temperature Converter by JC Boyd");
-        
-        Scanner stdIn = new Scanner(System.in);
-        String answer = "y";
-        String tempType = "f";
-        double userTemp = 0.0;
-        double result = 0.0;
-        
-        while (answer.equals("y"))
-        {
+        System.out.println("Temperature Converter by Kevin Bell");
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        String answer = "y", tempType = "f";
+        double userTemp = 0.0, result = 0.0;
+        while (answer.equals("y")) {
             System.out.print("\nWhat is the temperature to convert? ");
-            userTemp = Double.parseDouble(stdIn.nextLine());
+            userTemp = Double.parseDouble(computerKeyboardInput.nextLine());
             System.out.print("Is this Celsius (C) or Fahrenheit (F)? ");
-            tempType = stdIn.nextLine();
+            tempType = computerKeyboardInput.nextLine();
             tempType = tempType.toLowerCase();
-            
-            if (tempType.equals("c"))
-            {
+            if (tempType.equals("c")) {
                 result = CtoF(userTemp);
                 System.out.printf("Fahrenheit temp is %.2f\n", result);
-            }
-            else if (tempType.equals("f"))
-            {
+            } else if (tempType.equals("f")) {
                 result = FtoC(userTemp);
                 System.out.printf("Celsius temp is %.2f\n", result);
-            }
-            else
-            {
+            } else {
                 System.out.println("Error, use C or F for temperature type");
             } //end if
-            
             System.out.print("Do you have another temperature to convert? ");
-            answer = stdIn.nextLine().toLowerCase();
+            answer = computerKeyboardInput.nextLine().toLowerCase();
         } //end while
-        
     }//end main
 
     public static double FtoC(double Ftemp) {
@@ -62,8 +45,7 @@ public class TemperatureConverter {
     public static double CtoF(double Ctemp) {
         //converts incoming Celsius temp to Fahrenheit
         double Ftemp = 0.0;
-        Ftemp = (Ctemp * 9.0 / 5.0) +32;
+        Ftemp = (Ctemp * 9.0 / 5.0) + 32;
         return Ftemp;
     } //end CtoF
-    
 } //end class TemperatureConverter
